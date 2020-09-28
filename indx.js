402c6cc243a6de81ea41b3cbdaacfb704e8155f6
Quenton Radford <qdradford@outlook.com>
2020-09-27 18:56:40 -0700

    matrix diagnol sum solution

---
+++ indx.js
@@ -0,0 +1,30 @@
+const diagnolSum = (matrix) => {
+    var l2r = r2l = i = k = l = totalSum = 0;
+    var length = matrix.length - 1;
+    while (i < matrix.length && k < matrix.length && l >= 0) {
+        l2r += matrix[i][i];
+        r2l += matrix[k][length];
+
+        if(i == k && i == length){
+            totalSum -= matrix[i][i];
+        }
+        
+        i++;
+        k++;
+        length--;
+    }
+
+    totalSum += l2r;
+    totalSum += r2l;
+
+    console.log(totalSum);
+
+    return totalSum;
+}
+
+
+(() => {
+    var arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    diagnolSum(arr);
+})()
