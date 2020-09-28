52806e39efaef3c842d2b039d305ea0f7aec39fa
Quenton Radford <qdradford@outlook.com>
2020-09-27 18:59:35 -0700

    ..

--- index.js
+++ index.js
@@ -20,9 +20,6 @@ const diagnolSum = (matrix) => {
     return totalSum;
 }
 
+var arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-(() => {
-    var arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-    diagnolSum(arr);
-})()
+console.log(diagnolSum(arr));
